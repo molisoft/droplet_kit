b8db08fb65611a6ee9d4ddd566365428aff0849a
moli <molisoft@qq.com>
2015-04-01 01:24:24 +0800

    Update size.rb
    add size attributes

--- lib/droplet_kit/models/size.rb
+++ lib/droplet_kit/models/size.rb
@@ -1,8 +1,13 @@
 module DropletKit
   class Size < BaseModel
     attribute :slug
+    attribute :available
     attribute :transfer
     attribute :price_monthly
     attribute :price_hourly
+    attribute :memory
+    attribute :vcpus
+    attribute :disk
+    attribute :regions
   end
 end
