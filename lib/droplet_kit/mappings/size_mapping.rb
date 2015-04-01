2b22930eb4a977c5fbe2c9eb14710e0d898f518c
moli <molisoft@qq.com>
2015-04-01 10:41:53 +0800

    Update size_mapping.rb

--- lib/droplet_kit/mappings/size_mapping.rb
+++ lib/droplet_kit/mappings/size_mapping.rb
@@ -7,9 +7,15 @@ module DropletKit
       root_key singular: 'size', plural: 'sizes', scopes: [:read]
 
       property :slug, scopes: [:read]
+      property :available, scopes:[:read]
       property :transfer, scopes: [:read]
       property :price_monthly, scopes: [:read]
       property :price_hourly, scopes: [:read]
+      
+      property :memory, scopes: [:read]
+      property :vcpus, scopes: [:read]
+      property :disk, scopes: [:read]
+      property :regions, scopes: [:read]
     end
   end
 end
